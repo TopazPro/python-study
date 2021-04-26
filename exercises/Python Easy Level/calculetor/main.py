49c361c81dcd588bafbed6c65c73451c847f509f
TopazPro <topazh22@gmail.com>
2021-04-26 17:01:37 +0300

    added divided function

--- exercises/Python Easy Level/calculetor/main.py
+++ exercises/Python Easy Level/calculetor/main.py
@@ -6,6 +6,10 @@ def add(x, y):
     return x + y
 
 
+def subtraction(x, y):
+    return x - y
+
+
 def main():
     print(add(2, 2))
 
--- main.py
+++ main.py
@@ -9,3 +9,35 @@ print(f"Which is bigger? 5 or 9? {get_bigger(5,9)}")
 
 print("Exercise 2:")
 print(f"5 x 20 = {mul(5, 20)}")
+
+def addition(x, y):
+    return x + y
+
+
+def subtraction(x, y):
+    return x - y
+
+
+def divide(x, y):
+    return x / y
+
+
+def main():
+    print(mult(3, 4))
+
+
+
+def addition(x, y):
+    return x + y
+
+
+def subtraction(x, y):
+    return x - y
+
+
+def divide(x, y):
+    return x / y
+
+
+def main():
+    print(mult(3, 4))
